a38af0ebc1a2a6755dc3b54fc8eb776bed6d9c7d
Mayank Mishra <mayank31398@gmail.com>
2025-01-11 21:01:07 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/continuous_count_and_sort_test.py
+++ tests/kernels/continuous_count_and_sort_test.py
@@ -4,7 +4,7 @@ import torch
 from parameterized import parameterized
 from transformers import set_seed
 
-from cute_kernels import KernelBackend, continuous_count_and_sort_cute, continuous_count_torch
+from cute_kernels import continuous_count_and_sort_cute, continuous_count_torch
 
 from ..test_commons import TestCommons
 
