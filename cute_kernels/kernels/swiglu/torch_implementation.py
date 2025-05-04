dab5d8f6e499971dba6470e6f37afdfd68c9fd06
Mayank Mishra <mayank31398@gmail.com>
2025-05-04 18:52:36 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu/torch_implementation.py
+++ cute_kernels/kernels/swiglu/torch_implementation.py
@@ -4,3 +4,8 @@ import torch.nn.functional as F
 
 def swiglu_torch(gate: torch.Tensor, up: torch.Tensor) -> torch.Tensor:
     return up * F.silu(gate)
+
+
+def swiglu_packed_torch(x: torch.Tensor) -> torch.Tensor:
+    x = x.chunk(2, dim=-1)
+    return x[0] * F.silu(x[1])
