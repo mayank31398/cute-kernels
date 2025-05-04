0b64e7b62c738288caa84fd9349977350609b02e
Mayank Mishra <mayank31398@gmail.com>
2025-05-04 18:47:53 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ cute_kernels/kernels/swiglu/triton_implementation/forward/__init__.py
@@ -0,0 +1,2 @@
+from .packed import swiglu_packed_forward_triton
+from .unpacked import swiglu_forward_triton
---
+++ cute_kernels/kernels/swiglu/triton_implementation/forward/unpacked.py
@@ -0,0 +1,47 @@
+import torch
+import triton
+import triton.language as tl
+
+from .....constants import LIBRARY_NAME
+from .....math import ceil_divide
+from .....triton_math import sigmoid
+from .....utils import cute_op
+
+
+@triton.jit
+def _swiglu(gate_ptr, up_ptr, output_ptr, indices, mask):
+    gate = tl.load(gate_ptr + indices, mask=mask).to(tl.float32)
+    up = tl.load(up_ptr + indices, mask=mask)
+
+    output = up * gate * sigmoid(gate)
+    tl.store(output_ptr + indices, output, mask=mask)
+
+
+@triton.jit
+def swiglu_forward_triton_kernel(gate_ptr, up_ptr, output_ptr, N, BLOCK_SIZE: tl.constexpr):
+    BLOCK_ID = tl.program_id(axis=0)
+    NUM_BLOCKS = tl.num_programs(axis=0)
+
+    indices = BLOCK_ID * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+
+    if BLOCK_ID < NUM_BLOCKS - 1:
+        _swiglu(gate_ptr=gate_ptr, up_ptr=up_ptr, output_ptr=output_ptr, indices=indices, mask=None)
+    else:
+        _swiglu(gate_ptr=gate_ptr, up_ptr=up_ptr, output_ptr=output_ptr, indices=indices, mask=indices < N)
+
+
+@cute_op(f"{LIBRARY_NAME}::swiglu_forward_triton", mutates_args={"output"})
+def swiglu_forward_triton(
+    gate: torch.Tensor, up: torch.Tensor, output: torch.Tensor, BLOCK_SIZE: int, NUM_WARPS: int
+) -> None:
+    N = gate.numel()
+
+    with torch.device(gate.device):
+        swiglu_forward_triton_kernel[ceil_divide(N, BLOCK_SIZE),](
+            gate_ptr=gate,
+            up_ptr=up,
+            output_ptr=output,
+            N=N,
+            BLOCK_SIZE=BLOCK_SIZE,
+            num_warps=NUM_WARPS,
+        )
